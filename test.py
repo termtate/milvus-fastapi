c3bab11d1d4f63861ff316ddf6f22d19d6da3d73
termtate <meng360842@gmail.com>
2023-07-24 13:22:46 +0800

    "1.0"

---
+++ test.py
@@ -0,0 +1,4 @@
+from pprint import pprint
+
+a = {'param': {'metric_type': 'L2', 'index_type': 'FLAT'}, 'limit': 10, 'output_fields': ['id', 'id_card_number', 'name', 'hospitalize_num', 'case_number', 'sex', 'age', 'phone_number', 'seizure_evolution']} 
+b = {'param': {'metric_type': 'L2', 'index_type': 'FLAT'}, 'limit': 10}
