934b5cc0668c50718ebe5a1ed2adf814dfa40eff
termtate <meng360842@gmail.com>
2023-07-24 13:39:04 +0800

    1.1

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 ## 运行前准备
 
-1. 
+1. 安装rye
+   1. [下载rye](https://rye-up.com/guide/installation/)
+   2. [添加 `%USERPROFILE%\.rye\shims` 到Path环境变量](https://rye-up.com/guide/installation/#add-shims-to-path)
+2. 运行milvus
+   
+## 运行
+在当前目录下运行cmd命令：
+1. `rye sync`
+2. 如果是第一次运行，输入 `rye run python src/milvus_backend/initial_data.py` 初始化数据
+3. 
---
+++ src/milvus_backend/pre_start.py
@@ -0,0 +1,39 @@
+import logging
+
+from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
+from milvus.client import MilvusConnection
+from core.config import settings
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+max_tries = 60 * 5  # 5 minutes
+wait_seconds = 1
+
+
+@retry(
+    stop=stop_after_attempt(max_tries),
+    wait=wait_fixed(wait_seconds),
+    before=before_log(logger, logging.INFO),
+    after=after_log(logger, logging.WARN),
+)
+def init() -> None:
+    try:
+        with MilvusConnection(settings.milvus.HOST, settings.milvus.PORT) as conn:
+            collection = conn.get_collection(settings.milvus.COLLECTION_NAME)
+            with collection.load_data():
+                logger.info(f"entities number: {collection.collection.num_entities}")
+
+    except Exception as e:
+        logger.error(e)
+        raise e
+
+
+def main() -> None:
+    logger.info("Initializing service")
+    init()
+    logger.info("Service finished initializing")
+
+
+if __name__ == "__main__":
+    main()
