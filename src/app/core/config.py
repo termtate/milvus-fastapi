92b082085a85261ef479bbec22ca9abc6f2ee89a
termtate <meng360842@gmail.com>
2023-07-31 13:27:56 +0800

    add fields

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -19,9 +19,21 @@ class MilvusSettings(BaseSettings):
         FieldSchema(name="age", dtype=DataType.VARCHAR, max_length=50),
         FieldSchema(name="phone_number", dtype=DataType.VARCHAR, max_length=50),
         FieldSchema(name="seizure_evolution", dtype=DataType.VARCHAR, max_length=600),
+        FieldSchema(name="seizure_duration", dtype=DataType.VARCHAR, max_length=20),
+        FieldSchema(name="seizure_freq", dtype=DataType.VARCHAR, max_length=20),
+        FieldSchema(name="maternal_pregnancy_age", dtype=DataType.VARCHAR, max_length=20),
+        FieldSchema(name="pregnancy_num", dtype=DataType.VARCHAR, max_length=10),
+        FieldSchema(name="birth_weight", dtype=DataType.VARCHAR, max_length=20),
+        FieldSchema(name="head_c", dtype=DataType.VARCHAR, max_length=15),
+        FieldSchema(name="blood_urine_screening", dtype=DataType.VARCHAR, max_length=30),
+        FieldSchema(name="copper_cyanin", dtype=DataType.VARCHAR, max_length=25),
+        FieldSchema(name="csf", dtype=DataType.VARCHAR, max_length=25),
+        FieldSchema(name="genetic_test", dtype=DataType.VARCHAR, max_length=25),
+        FieldSchema(name="head_ct", dtype=DataType.VARCHAR, max_length=25),
+        FieldSchema(name="head_mri", dtype=DataType.VARCHAR, max_length=25),
+        FieldSchema(name="scalp_eeg", dtype=DataType.VARCHAR, max_length=25),
+        FieldSchema(name="precipitating_factor", dtype=DataType.VARCHAR, max_length=300),
     ]
-    
-    # EMBEDDING_FIELD_NAME = "seizure_evolution"
     
     # https://milvus.io/docs/v2.0.x/build_index.md#Prepare-index-parameter
     VECTOR_FIELD_INDEX_PARAMS = {
@@ -30,7 +42,8 @@ class MilvusSettings(BaseSettings):
     }
     
     VECTOR_FIELDS: list[str] = [
-        "seizure_evolution",                                        
+        "seizure_evolution",   
+        # "precipitating_factor"                                     
     ]
 
 
